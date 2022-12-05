83e3958961b9ac6956ea893cb5c4ae87a363f1e6
Diego Fincatto <diego@fincatto.com>
2022-12-05 15:32:27 -0300

    Adicionado teste de banco de dados.

--- src/main/java/com/fincatto/vaadintest/views/HomeView.java
+++ src/main/java/com/fincatto/vaadintest/views/HomeView.java
@@ -1,14 +1,22 @@
 package com.fincatto.vaadintest.views;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H4;
 import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.PasswordField;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.VaadinSession;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -49,6 +57,42 @@ public class HomeView extends VerticalLayout {
         HorizontalLayout horizontalLayout = new HorizontalLayout(button, spanCounter);
         horizontalLayout.setDefaultVerticalComponentAlignment(Alignment.CENTER);
         this.add(horizontalLayout);
+
+        //monta teste de conexao
+        final TextField tfUrl = new TextField("URL", "jdbc:postgresql://localhost:port/dbname");
+        final TextField tfUser = new TextField("Username");
+        final PasswordField pfPass = new PasswordField("Password");
+        final Button btnConnect = new Button("Connect", el -> {
+            try {
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException e) {
+                System.err.println("Erro ao carregar driver: " + e.getLocalizedMessage());
+                final Notification notification = new Notification();
+                notification.setText(e.getLocalizedMessage());
+                notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+                notification.setPosition(Notification.Position.BOTTOM_START);
+                notification.setDuration(10000);
+                notification.open();
+            }
+            try (final Connection connection = DriverManager.getConnection(tfUrl.getValue(), tfUser.getValue(), pfPass.getValue())) {
+                System.out.println("Sucesso: " + connection.toString());
+                final Notification notification = new Notification();
+                notification.setText(connection.toString());
+                notification.setDuration(5000);
+                notification.open();
+            } catch (SQLException e) {
+                System.err.println("Erro: " + e.getLocalizedMessage());
+                final Notification notification = new Notification();
+                notification.setText(e.getLocalizedMessage());
+                notification.addThemeVariants(NotificationVariant.LUMO_ERROR);
+                notification.setPosition(Notification.Position.BOTTOM_START);
+                notification.setDuration(10000);
+                notification.open();
+            }
+        });
+        this.add(new FormLayout(tfUrl, tfUser, pfPass, btnConnect));
+
+        //configura pagina
         this.setSpacing(false);
     }
 }
